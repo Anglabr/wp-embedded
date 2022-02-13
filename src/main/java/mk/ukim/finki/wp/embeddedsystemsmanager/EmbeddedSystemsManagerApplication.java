4f053c3ab8a4ccde0bed273413bab5cd8b35ea3f
Anglabr <angelina.labroska@gmail.com>
2022-02-13 19:41:05 +0100

    initial commit

---
+++ src/main/java/mk/ukim/finki/wp/embeddedsystemsmanager/EmbeddedSystemsManagerApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.embeddedsystemsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedSystemsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddedSystemsManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/wp/embeddedsystemsmanager/EmbeddedSystemsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.embeddedsystemsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedSystemsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
